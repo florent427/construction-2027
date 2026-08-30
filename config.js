ed886e5dedb0853bea4d4ab9bc0aaa69846ae54f
florent427 <53866042+florent427@users.noreply.github.com>
2026-08-30 11:20:41 +0400

    Add public Supabase config

---
+++ config.js
@@ -0,0 +1,9 @@
+window.APP_CONFIG = {
+  supabaseUrl: "https://muyaahtgkjmretyvtoyt.supabase.co",
+  supabasePublishableKey: "sb_publishable_tZ4hZQz11ESter4aUJs84w_Jx4I9w8u",
+  projectId: "construction-2027",
+  authorizedMembers: [
+    { name: "Florent" },
+    { name: "Jimmy" }
+  ]
+};
